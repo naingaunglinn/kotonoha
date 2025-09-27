02581e289cdfc5b513cdcb4cccc134183c64cf18
naingaunglinn <naingaunglinn369@gmail.com>
2025-09-27 23:39:18 +0630

    update [home]: logo url added

--- src/app/components/layout/Header.tsx
+++ src/app/components/layout/Header.tsx
@@ -1,13 +1,14 @@
 import {User} from "lucide-react";
+import Link from "next/link";
 
 const AppHeader = () => (
   <header className="bg-[#F5EDED]/80 backdrop-blur-md sticky top-0 border-b border-[#3E3636]/10 z-20">
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div className="flex items-center justify-between h-20">
         <div className="flex items-center">
-          <span className="text-3xl font-bold text-[#3E3636] tracking-tight">
+          <Link href={"/"} className="text-3xl font-bold text-[#3E3636] tracking-tight">
             <span className="text-[#D72323]">葉</span> Konotoha
-          </span>
+          </Link>
         </div>
         <div className="flex items-center">
           <button className="p-3 rounded-full hover:bg-[#3E3636]/10 transition-colors duration-300">
